671368e527eebd3546f837ff4a0e0154ffcaac5a
Miloš Lapiš <mlapis@mlc.cz>
2024-11-30 14:17:02 +0100

    wip(zerops): Skip verify email transport

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -100,7 +100,7 @@ export default buildConfig({
       host: 'mailpit',
       port: 1025,
       secure: false,
-      skipVerify: false,
+      skipVerify: true,
       logger: true,
       // debug: true
     }
